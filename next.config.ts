04793a1fc272317abd8bc34ac8c87d176842632f
Henry A. Lawal <henrynnovate@gmail.com>
2026-09-01 03:04:57 +0100

    chore: update pnpm workspace overrides for dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  allowedDevOrigins: ['10.203.188.92']
+  
 };
 
 export default nextConfig;
